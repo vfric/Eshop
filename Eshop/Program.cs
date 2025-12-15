13402ee06fbf1c0c91e6601e4b622a7d66c4ceb1
Vojtěch Frič <vojtafric1@gmail.com>
2025-12-15 11:07:41 +0100

    chore: initial project setup with api versioning and custom swagger

--- Eshop/Program.cs
+++ Eshop/Program.cs
@@ -1,23 +1,80 @@
+using ApiVersioning.Examples;
+using Asp.Versioning;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+[assembly: Microsoft.AspNetCore.Mvc.ApiController]
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddProblemDetails();
+builder.Services.AddApiVersioning(
+                    options =>
+                    {
+                        // reporting api versions will return the headers
+                        // "api-supported-versions" and "api-deprecated-versions"
+                        options.ReportApiVersions = true;
+
+                        options.Policies.Sunset(0.9)
+                                        .Effective(DateTimeOffset.Now.AddDays(60))
+                                        .Link("policy.html")
+                                            .Title("Versioning Policy")
+                                            .Type("text/html");
+                    })
+                .AddMvc()
+                .AddApiExplorer(
+                    options =>
+                    {
+                        // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                        // note: the specified format code will format the version as "'v'major[.minor][-status]"
+                        options.GroupNameFormat = "'v'VVV";
+
+                        // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                        // can also be used to control the format of the API version in route templates
+                        options.SubstituteApiVersionInUrl = true;
+                    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+builder.Services.AddSwaggerGen(
+    options =>
+    {
+        // add a custom operation filter which sets default values
+        options.OperationFilter<SwaggerDefaultValues>();
+
+        var fileName = typeof(Program).Assembly.GetName().Name + ".xml";
+        var filePath = Path.Combine(AppContext.BaseDirectory, fileName);
+
+        // integrate xml comments
+        options.IncludeXmlComments(filePath);
+    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseSwagger();
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(
+       options =>
+       {
+           var descriptions = app.DescribeApiVersions();
+
+           // build a swagger endpoint for each discovered API version
+           foreach (var description in descriptions)
+           {
+               var url = $"/swagger/{description.GroupName}/swagger.json";
+               var name = description.GroupName.ToUpperInvariant();
+               options.SwaggerEndpoint(url, name);
+           }
+       });
 }
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ Eshop/V1/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Eshop.Models;
+
+namespace Eshop.V1.Controllers;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/products")]
+public class ProductsController : ControllerBase
+{
+    // MOCK DATA
+    private static readonly List<Product> Products =
+    [
+        new Product
+        {
+            Id = 1,
+            Name = "Product A",
+            ImgUrl = "https://example.com/a.jpg",
+            Price = 199.90m,
+            Description = "First test product"
+        },
+        new Product
+        {
+            Id = 2,
+            Name = "Product B",
+            ImgUrl = "https://example.com/b.jpg",
+            Price = 299.90m,
+            Description = "Second test product"
+        }
+    ];
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> GetAll()
+    {
+        return Ok(Products);
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Product> GetById(int id)
+    {
+        var product = Products.FirstOrDefault(p => p.Id == id);
+
+        if (product == null)
+            return NotFound();
+
+        return Ok(product);
+    }
+}
